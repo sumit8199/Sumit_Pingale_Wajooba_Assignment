9e5826b4053c12b5e9aa0cbd592fe2ee9cad4266
sumit8199 <98594450+sumit8199@users.noreply.github.com>
2024-02-04 18:35:23 +0530

    Update crudModel.js

--- server/models/crudModel.js
+++ server/models/crudModel.js
@@ -3,17 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 	name: {
 		type: String,
-		////required: [true, "Company Name is required"]
 	},
 	phone: {
 		type: String,
-		//required: [true, "User phone number required"]
 	},
 	email: {
 		type: String,
-		// required: [true, "Email is required"],
-		// trim: true,
-		// unique: [true, "Email already exists"],
 	},
 });
 
